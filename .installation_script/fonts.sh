4fc4cd5a77d286bcb29636e85357fc851dc57fed
Patriot720 <cerkin-3@yandex.ru>
2021-09-21 10:17:05 +0300

    rofi fonts

--- .installation_script/fonts.sh
+++ .installation_script/fonts.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
+fonts_dir=$home_dir/.local/share/fonts/
+
 install_shure_font(){
     wget -O shure_mono.ttf https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/ShareTechMono/complete/Shure%20Tech%20Mono%20Nerd%20Font%20Complete.ttf?raw=true
     mv shure_mono.ttf $home_dir/.local/share/fonts/
     fc-cache
 }
 
+install_rofi_fonts(){
+    git clone https://github.com/adi1090x/rofi;
+    cd rofi;
+    cp -rf fonts/* "$fonts_dir";
+    cd ..;
+    rm -rf rofi;
+}
+
 if prompt_default_yes "Install fonts?"; then
     yay -Sy ttf-font-awesome gnu-free-fonts ttf-nerd-fonts-symbols-mono ttf-weather-icons ttf-comfortaa ttf-arphic-uming \
         adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts ttf-baekmuk otf-ipafont noto-fonts ttf-fira-code \
         ttf-hanazono;
-    mkdir -p .local/share/fonts/
+    mkdir -p $fonts_dir
     install_shure_font;
+    install_rofi_fonts;
 fi
