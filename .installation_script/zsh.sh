e9406f48410807651780f1aacb8f10e716563230
Patriot720 <cerkin-3@yandex.ru>
2021-09-21 09:34:20 +0300

    fix ohmyzsh zshrc auto-removal

--- .installation_script/zsh.sh
+++ .installation_script/zsh.sh
@@ -3,7 +3,7 @@
 install_zsh(){
     yay -Sy zsh
     chsh -s /bin/zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 }
 
 if prompt "Install zsh? (Should be run last)"; then
