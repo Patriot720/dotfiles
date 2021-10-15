cf46d127b840bb12ec140aff376cd003a73621e9
Patriot720 <cerkin-3@yandex.ru>
2021-10-15 10:03:28 +0300

    Update 10:03 Fri, 15 Oct

--- .config/sway/vid_region.sh
+++ .config/sway/vid_region.sh
@@ -5,6 +5,6 @@ if pgrep -x wf-recorder > /dev/null
     echo -e "file:///tmp/vid.mp4" | wl-copy -t text/uri-list
     notify-send "Video recorded"
 else
-    wf-recorder -g (slurp) -m mp4 -f /tmp/vid.mp4;
+    wf-recorder -g (slurp) -m mp4 -f /tmp/vid.mp4 &;
     notify-send "Started recording"
 end
