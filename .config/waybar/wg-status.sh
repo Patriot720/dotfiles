b3c932816dfc8cdc7dbd21b388e10f907f69b646
Patriot720 <cerkin-3@yandex.ru>
2021-11-06 20:14:50 +0300

    update wg-status

--- .config/waybar/wg-status.sh
+++ .config/waybar/wg-status.sh
@@ -1,8 +1,21 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 # requires wg-quick to be in sudoers file
-alias wg-generate="cd $HOME;wgcf register --accept-tos; wgcf generate;"
-alias wg-start="cd $HOME;sudo wg-quick up wgcf-profile.conf"
-alias wg-stop="cd $HOME;sudo wg-quick down wgcf-profile.conf"
+
+wg-generate(){
+    cd $HOME;
+    wgcf register --accept-tos;
+    wgcf generate;
+}
+
+wg-start(){
+    cd $HOME;
+    sudo wg-quick up wgcf-profile.conf
+}
+
+wg-stop(){
+    cd $HOME;
+    sudo wg-quick down wgcf-profile.conf
+}
 
 is_off(){
     wg_status=$(wg show 2>&1);
@@ -26,12 +39,11 @@ wg-toggle(){
             wg-generate;
         fi
         wg-start
-        if ping -c 2 google.com; then
-        else
+        until ping -c 2 google.com; do
             wg-stop;
             wg-generate;
             wg-start;
-        fi
+        done
     else
         wg-stop
     fi
