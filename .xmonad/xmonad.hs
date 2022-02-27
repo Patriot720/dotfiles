d2a9171ec5c6ff40a356f69a52cfcf006cac582c
Patriot720 <cerkin-3@yandex.ru>
2022-02-27 22:00:04 +0300

    Update 22:00 Sun, 27 Feb

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import Control.Monad (forM_)
+import Data.List.Split (splitOn)
 import qualified Data.Map as M
 import Data.Traversable (for)
 import System.Taffybar.Support.PagerHints
@@ -12,17 +13,20 @@ import qualified XMonad.Hooks.Focus as W
 import XMonad.Hooks.InsertPosition
 import XMonad.Hooks.ManageDocks
 import qualified XMonad.Layout.AutoMaster as XLA
+import XMonad.Layout.Drawer (Property (ClassName, Or), drawer, onRight, onTop, simpleDrawer)
 import XMonad.Layout.Grid
 import XMonad.Layout.IndependentScreens
 import qualified XMonad.Layout.IndependentScreens as W
 import XMonad.Layout.Magnifier (magnifier)
 import XMonad.Layout.NoBorders
+import XMonad.Layout.StackTile (StackTile (StackTile))
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.TwoPane (TwoPane (TwoPane))
 import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
 import XMonad.Util.SpawnOnce
 import XMonad.Util.Ungrab
+import XMonad.Util.WorkspaceCompare (getSortByXineramaRule)
 
 myConfig =
   def
@@ -42,6 +46,9 @@ myConfig =
               -- "/usr/lib/kdeconnectd",
               -- "~/.dropbox-dist/dropboxd",
               -- "dunst",
+              -- "env LD_PRELOAD=/usr/lib/spotify-adblock.so spotify %U",
+              -- "telegram-desktop",
+              -- "flameshot",
               -- "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1",
               -- "blueman-applet",
               -- "fcitx -d"
@@ -62,8 +69,11 @@ myConfig =
           ]
     }
 
-layout = lessBorders Screen (avoidStruts tiled)
+layout = lessBorders Screen (avoidStruts (tiled ||| Full))
   where
+    consoleOn i = simpleDrawer 0 0.5 (ClassName "Nautilus" `Or` ClassName "Xchat") `onRight` i
+    testLayout = consoleOn (Tall 1 0.03 0.5)
+    stackTile = StackTile 1 (3 / 100) (1 / 2)
     tiled = Tall nmaster delta ratio
     threeCol = ThreeColMid nmaster delta ratio
     nmaster = 1
@@ -80,7 +90,17 @@ myKeysP =
     ("F12", spawn "guake-toggle"),
     ("M-0", windows $ W.view "10"),
     ("M-M1-h", sendMessage Shrink),
-    ("M-M1-l", sendMessage Expand)
+    ("M-M1-l", sendMessage Expand),
+    ("XF86AudioRaiseVolume", spawn "pactl set-sink-volume @DEFAULT_SINK@ +10% && $refresh_i3status"),
+    ("XF86AudioLowerVolume", spawn "pactl set-sink-volume @DEFAULT_SINK@ -10% && $refresh_i3status"),
+    ("XF86AudioMute", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status"),
+    ("XF86AudioMicMute", spawn "pactl set-source-mute @DEFAULT_SOURCE@ toggle && $refresh_i3status"),
+    ("XF86MonBrightnessUp", spawn "brightnessctl s +10%"),
+    ("XF86MonBrightnessDown", spawn "brightnessctl s 10%-"),
+    ("XF86AudioPlay", spawn "--no-startup-id dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"),
+    ("XF86AudioPause", spawn "--no-startup-id dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause"),
+    ("XF86AudioNext", spawn "--no-startup-id dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"),
+    ("XF86AudioPrev", spawn "--no-startup-id dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
   ]
 
 shiftThenView i = W.view i . W.shift i
@@ -95,12 +115,18 @@ myKeys =
            (f, m) <- [(W.view, 0), (shiftThenView, shiftMask)]
        ]
 
+mySort = getSortByXineramaRule
+
+myRename :: String -> WindowSpace -> String
+myRename s _w = last $ splitOn "_" s
+
 main :: IO ()
 main = do
   xmonad $
     docks $
       ewmhFullscreen $
-        ewmh $
-          pagerHints $
-            myConfig `additionalKeysP` myKeysP
-              `additionalKeys` myKeys
+        addEwmhWorkspaceRename (pure myRename) $
+          ewmh $
+            pagerHints $
+              myConfig `additionalKeysP` myKeysP
+                `additionalKeys` myKeys
