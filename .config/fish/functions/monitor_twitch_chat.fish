7356e32ad301254c38fd0a9e8b7fde171f77942d
Patriot720 <cerkin-3@yandex.ru>
2022-06-06 22:00:05 +0300

    Update 22:00 Mon, 06 Jun

---
+++ .config/fish/functions/monitor_twitch_chat.fish
@@ -0,0 +1,3 @@
+function monitor_twitch_chat
+script -q /dev/null -c "tcc connect patriot720" > ~/build/twitch-chat-cli/test.txt
+end
---
+++ .config/fish/functions/start_coding_stream.fish
@@ -0,0 +1,5 @@
+function start_coding_stream
+    screenkey_all_screens
+    monitor_twitch_chat
+end
+
